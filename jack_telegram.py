a531d6e7b37eaf91fa08d7e7267fffe6df233658
Dimitri <dima@jack.local>
2026-08-21 05:36:08 +0200

    Voice: Doppel-VORSCHLAG entfernt (APPCMD-Pfad exklusiv)

--- jack_telegram.py
+++ jack_telegram.py
@@ -1099,9 +1099,20 @@ def main():
                                 ans = ''
                                 rw2 = None
                             else:
+                                import jack_exec_parser
+                                ans = jack_exec_parser.parse_and_prepare(str(ans), PENDING_EXEC, send_keyboard) or ''
+                            if isinstance(ans, str) and ans.startswith('__APPCMD__:'):
+                                _pkg = ans.split(':', 1)[1]
+                                _cmd = 'ssh xiaomi-jack "su -c \'monkey -p ' + _pkg + ' -c android.intent.category.LAUNCHER 1\'"'
+                                PENDING_EXEC.clear()
+                                PENDING_EXEC['cmd'] = _cmd
+                                send_keyboard('VORSCHLAG:' + chr(10) + _cmd, [[('🟢 Ausfuehren', 'run_exec'), ('🔴 Abbrechen', 'cancel_exec')]])
+                                send('Du: ' + str(heard) + chr(10) + chr(10) + 'JACK: App-Befehl erkannt - Freigabe tippen.')
+                                ans = ''
+                                rw2 = None
+                            else:
                                 pass
                             import jack_exec_parser
-                            ans = jack_exec_parser.parse_and_prepare(str(ans), PENDING_EXEC, send_keyboard) or ""
                             import jack_intent_apps
                             if jack_intent_apps.try_app_launch(str(heard), PENDING_EXEC, send_keyboard):
                                 send("Du: "+str(heard)+chr(10)+chr(10)+"JACK: App-Befehl bereit - Freigabe tippen.")
